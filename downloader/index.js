0c0ad8151a949d248db456b7a73765aa29c8ed71
Daniel Llewellyn <daniel@bowlhat.net>
2020-03-09 20:15:01 +0000

    Update downloader to support language selection
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- downloader/index.js
+++ downloader/index.js
@@ -1,3 +1,4 @@
+let {exec}  = require('child_process')
 let fetch = require('node-fetch')
 
 let now = (new Date).getTime()
@@ -14,17 +15,37 @@ fetch(`https://www.microsoft.com/en-us/software-download/windows10ISO`)
     const step1_uuid = RegExp.$3
     const step2_uuid = RegExp.$7
     return fetch(`https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=${step1_uuid}&host=www.microsoft.com&segments=software-download%2cwindows10ISO&query=&action=getskuinformationbyproductedition&sessionId=${session_id}&productEditionId=1429&sdVersion=2`)
-        .then(res => 
-            fetch(`https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=${step2_uuid}&host=www.microsoft.com&segments=software-download%2cwindows10ISO&query=&action=GetProductDownloadLinksBySku&sessionId=${session_id}&skuId=9030&language=English%20International&sdVersion=2`)
+        .then(res => res.text())
+        .then(step1_text => [...step1_text.matchAll(/<option value=['"]([^"']+)['"]>([^<])+<\/option>/g)])
+        .then(languages => (
+            languages
+                .map(language => language[1].replace(/&quot;/g, '"'))
+                .map(JSON.parse)
+        ))
+        .then(languages => {
+            return new Promise((resolve, reject) => {
+                let prompt = exec(`yad --entry --center --title='Choose language' --entry-label=Language ${languages.map(lang => `"${lang.language}"`).join(' ')}`, (error, stdout, stderr) => {
+                    if (error) {
+                        reject('Cancelled the operation')
+                    }
+                    resolve(stdout.trim())
+                })
+            })
+            .then(language => languages.find(el => el.language === language))
+        })
+        .then(language =>
+            fetch(`https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=${step2_uuid}&host=www.microsoft.com&segments=software-download%2cwindows10ISO&query=&action=GetProductDownloadLinksBySku&sessionId=${session_id}&skuId=${language.id}&language=${encodeURIComponent(language.language)}&sdVersion=2`)
+            .then(res => res.text())
+            .then(body => [
+                ...body.matchAll(/<input type="hidden" class="product-download-hidden" value="([^"]+)"/g)]
+                .map(download => download[1].replace(/&quot;/g, `"`).replace(/(IsoX86|IsoX64)/g, `"$1"`))
+                .map(JSON.parse)
+                .find(download => download.DownloadType === 'IsoX64')
+            )
         )
 })
-.then(res => res.text())
-.then(body => {
-    body.match(/<a [^>]*href=(['"])(https:\/\/software-download.microsoft.com\/[^'"]*Win10_[0-9]{4,}_EnglishInternational_x64.iso[^'"]*)\1/)
-    return RegExp.$2
-})
-.then(url => {
-    console.log(url.replace(/&amp;/g, '&'))
+.then(download => {
+    console.log(download.Uri.replace(/&amp;/g, '&'))
     process.exitCode = 0
 })
 .catch(e => {
