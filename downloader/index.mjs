b3e9cd8733b4bf04c58c2deb9ffc0a37d2467e24
Daniel Llewellyn <diddledan@ubuntu.com>
2021-03-24 23:42:51 +0000

    Update downloader
    * Enable the downloader to fetch the latest build of Windows 10 when it updates
    
    Signed-off-by: Daniel Llewellyn <diddledan@ubuntu.com>

---
+++ downloader/index.mjs
@@ -0,0 +1,52 @@
+import { exec } from 'child_process';
+import fetch from 'node-fetch'
+
+(async function() {
+    try {
+        let now = (new Date).getTime()
+        let session_id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (t) => {
+            let i = (now + Math.random() * 16) % 16 | 0;
+            return now = Math.floor(now / 16),
+            (t == 'x' ? i : i & 3 | 8).toString(16)
+        })
+
+        let res = await fetch(`https://www.microsoft.com/en-us/software-download/windows10ISO`)
+        let body = await res.text()
+
+        body.match(/<div[^>]*id=(['"])SoftwareDownload_LanguageSelectionByProductEdition\1 [^>]*data-defaultPageId=(['"])([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})\2[^>]*>.*<div[^>]*id=(['"])SoftwareDownload_DownloadLinks\5 [^>]*data-defaultPageId=(['"])([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})\6/)
+        const step1_uuid = RegExp.$3
+        const step2_uuid = RegExp.$7
+        body.match(/<select[^>]*id=(['"])product-edition\1[^>]*><option value="" selected="selected">[^<]*<\/option>(?:<optgroup label=(["'])Windows 10[^>]*\2>)<option value=(["'])(\d+)\3>/)
+        const product_id = RegExp.$4
+
+        res = await fetch(`https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=${step1_uuid}&host=www.microsoft.com&segments=software-download%2cwindows10ISO&query=&action=getskuinformationbyproductedition&sessionId=${session_id}&productEditionId=${product_id}&sdVersion=2`)
+        let step1_text = await res.text()
+        let languages = [...step1_text.matchAll(/<option value=['"]([^"']+)['"]>([^<])+<\/option>/g)]
+        languages = languages
+            .map(language => language[1].replace(/&quot;/g, '"'))
+            .map(JSON.parse)
+        
+        let language = await new Promise((resolve, reject) => {
+            exec(`yad --entry --center --title='Choose language' --entry-label=Language ${languages.map(lang => `"${lang.language}"`).join(' ')}`, (error, stdout, stderr) => {
+                if (error) {
+                    reject('Cancelled the operation')
+                } else {
+                    resolve(stdout.trim())
+                }
+            })
+        })
+        language = languages.find(el => el.language === language)
+        res = await fetch(`https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=${step2_uuid}&host=www.microsoft.com&segments=software-download%2cwindows10ISO&query=&action=GetProductDownloadLinksBySku&sessionId=${session_id}&skuId=${language.id}&language=${encodeURIComponent(language.language)}&sdVersion=2`)
+        body = await res.text()
+        let download = [...body.matchAll(/<input type="hidden" class="product-download-hidden" value="([^"]+)"/g)]
+            .map(download => download[1].replace(/&quot;/g, `"`).replace(/(IsoX86|IsoX64)/g, `"$1"`))
+            .map(JSON.parse)
+            .find(download => download.DownloadType === 'IsoX64')
+
+        console.log(download.Uri.replace(/&amp;/g, '&'))
+        process.exitCode = 0
+    } catch (e) {
+        console.error(e)
+        process.exitCode = 1
+    }
+}())
