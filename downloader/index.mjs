bee6cf51bf5e5729ab2533ebcbf444c77c604599
Dani Llewellyn <diddledani@ubuntu.com>
2022-08-03 00:26:44 +0100

    Fix broken installer
    Signed-off-by: Dani Llewellyn <diddledani@ubuntu.com>

--- downloader/index.mjs
+++ downloader/index.mjs
@@ -30,10 +30,10 @@ import fetch from 'node-fetch'
         let res = await fetch(`https://www.microsoft.com/en-us/software-download/${sku}`)
         let body = await res.text()
 
-        body.match(/<div[^>]*id=(['"])SoftwareDownload_LanguageSelectionByProductEdition\1 [^>]*data-defaultPageId=(['"])([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})\2[^>]*>.*<div[^>]*id=(['"])SoftwareDownload_DownloadLinks\5 [^>]*data-defaultPageId=(['"])([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})\6/)
+        body.match(/<div[^>]*id=(['"])SoftwareDownload_LanguageSelectionByProductEdition\1 [^>]*data-defaultPageId=(['"])([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})\2[^>]*>.*<div[^>]*id=(['"])SoftwareDownload_DownloadLinks\5 [^>]*data-defaultPageId=(['"])([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})\6/i)
         const step1_uuid = RegExp.$3
         const step2_uuid = RegExp.$7
-        body.match(/<select[^>]*id=(['"])product-edition\1[^>]*><option value="" selected="selected">[^<]*<\/option>(?:(?:<!--)?<optgroup [^>]*>(?:-->)?)?<option value=(["'])(\d+)\2>/)
+        body.match(/<select[^>]*id=(['"])product-edition\1[^>]*><option value="" selected="selected">[^<]*<\/option>(?:(?:<!--)?<?optgroup[^>]*>?(?:-->)?)<option value=(['"])(\d+)\2[^>]*>/i)
         const product_id = RegExp.$3
 
         res = await fetch(`https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=${step1_uuid}&host=www.microsoft.com&segments=software-download%2c${sku}&query=&action=getskuinformationbyproductedition&sessionId=${session_id}&productEditionId=${product_id}&sdVersion=2`)
